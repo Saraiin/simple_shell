5f6aeb8adb62f9a859e2c3a637119c6c24253569
samad <abdessamad.bouchelga@gmail.com>
2023-05-20 17:05:09 -0700

    for string

---
+++ forstring.c
@@ -0,0 +1,38 @@
+#include "lshell.h"
+/**
+ * str_len - return length of a string
+ * @s: string to check
+ * Return: length of string
+ */
+int str_len(char *s)
+{
+	int i = 0, length = 0;
+
+	while (s[i++])
+		length++;
+	return (length);
+}
+/**
+ * str_dup - return pointer to duplicated string
+ * @s: string to duplicate
+ * Return: pointer to the new string
+ */
+char *str_dup(char *s)
+{
+	char *duplicatedS;
+	int len = 0, i;
+
+	if (s == NULL)
+		return (NULL);
+	len = str_len(s);
+	duplicatedS = malloc(sizeof(char) * (len + 1));
+	if (duplicatedS == NULL)
+	{
+		perror("Error Malloc");
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+		duplicatedS[i] = s[i];
+	duplicatedS[i] = '\0';
+	return (duplicatedS);
+}
---
+++ lshell.h
@@ -0,0 +1,13 @@
+#ifndef LSHELL_H
+#define LSHELL_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+/* for string */
+char *str_dup(char *s);
+char *str_dup(char *s)
+
+#endif
